3ce4613f6cb33fc70485c53049e7c726258b88c5
jademonroe06 <mikaelajael777@gmail.com>
2026-01-14 12:48:33 +0100

    Parte 2 Actividad PMDM

--- app/src/main/java/es/fpsumma/dam2/api/data/remote/RetrofitClient.kt
+++ app/src/main/java/es/fpsumma/dam2/api/data/remote/RetrofitClient.kt
@@ -1,17 +1,25 @@
 package es.fpsumma.dam2.api.data.remote
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import es.fpsumma.dam2.api.data.remote.api.TareaAPI
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:3000/" //preguntar que puerto hay que poner
+    private const val BASE_URL = "http://10.0.2.2:3000/"
+
+    //instancia de Moshi con el adaptador de Kotlin
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            //se pasa nuestra instancia de moshi configurada al conversor
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
